fc3c02259baab739f9b45f2f1b4de09b813373c0
João Aquino <joao.aquino.dev@gmail.com>
2021-02-26 21:19:50 -0300

    Add icon to buttons

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -43,6 +43,8 @@ export function Countdown() {
               onClick={resetCountdown}
             >
               Abandonar ciclo
+              <img src="icons/close-button.svg" />
+              <img src="icons/close-button-black.svg" />
             </button>
           ) : (
             <button
@@ -51,6 +53,7 @@ export function Countdown() {
               onClick={startCountdown}
             >
               Iniciar um ciclo
+              <img src="icons/play.svg" />
             </button>
           )}
         </>
--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -27,7 +27,7 @@ let countdownTimeout: NodeJS.Timeout;
 export function CountdownProvider({ children }: CountdownProviderProps) {
   const { startNewChallenge } = useContext(ChallengesContext);
 
-  const [time, setTime] = useState(0.05 * 60);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinish, setHasFinish] = useState(false);
 
@@ -41,7 +41,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
   function resetCountdown() {
     clearTimeout(countdownTimeout);
     setIsActive(false);
-    setTime(0.05 * 60);
+    setTime(25 * 60);
     setHasFinish(false);
   }
 
